93af872f967f91272ee59f3615757aa8ab006dbf
Walter Cyrulik <wcyrulik@cox.net>
2021-08-15 20:46:37 -0400

    Changes to implement objects
    The instructors introduced the use of objects to allow for the transfer of more complex data types in our javascript functions.  These changes represent the inclusion of these concepts.

--- js/site.js
+++ js/site.js
@@ -5,48 +5,46 @@ function getValue() {
     document.getElementById("alert").classList.remove("alert-danger");
     document.getElementById("alert").classList.remove("alert-success");
     let userString = document.getElementById("userString").value;
-    userString = removeUnwantedChars(userString).toLowerCase();
     if (userString.length > 0){
-        let revString = reverseString(userString);
-        displayString(revString, userString);
+        let returnObj = checkForPalindrome(userString);
+        displayString(returnObj);
     } else {
         alert('You must enter a word, phrase or sentence.  Try again.');
     }
 }
+//The Coder Foundry solution
+function checkForPalindrome(userString){
+    //Set the userString to lower case.
+    userString = userString.toLowerCase();
 
-//Reverse the string
-function reverseString(userString){
-    let revString = "";
+    //Remove spaces and special characters
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex,'');
+
+    let revString = [];
+    let returnobj = {};
 
     for (let index = userString.length - 1; index >= 0; index--) {
         revString += userString[index]; 
     }
-    return revString;
-}
-
-function removeUnwantedChars(userString) {
-    let cleanString = '';
-    let letters = /^[A-Za-z]+$/;
-
-    for (let i = 0; i <= userString.length -1; i++){
-        if (userString[i].match(letters)){
-            cleanString += userString[i];
-        }
+    if (revString == userString) {
+        returnobj.msg = "Well done!  You entered a palindrome!";
+        returnobj.alertc = "alert-success";
+    } else {
+        returnobj.msg = "Sorry!  You did not enter a palimdrome.";
+        returnobj.alertc = "alert-danger";
     }
-    return cleanString;
+    returnobj.reversed = revString;
+    returnobj.userstring = userString;
+
+    return returnobj;
 }
 
 //Display the reversed string back on the page.
-function displayString(revstring, userstring){
-    if (revstring == userstring) {
-        document.getElementById("alert-header").innerHTML = 'Sccess!!';
-        document.getElementById("msg").innerHTML = `Your string <strong>${userstring}</strong> is a palindrome`;
-        document.getElementById("alert").classList.add("alert-success");
-    } else {
-        document.getElementById("alert-header").innerHTML = 'Sorry ';
-        document.getElementById("msg").innerHTML = `Your string <strong>${userstring}</strong> is NOT a palindrome`;
-        document.getElementById("alert").classList.add("alert-danger");
-    }
+function displayString(returnObj){
+    document.getElementById("alert-header").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML 
+        = `Your string reversed is: <strong>${returnObj.reversed}</strong>.`;
+    document.getElementById("alert").classList.add(returnObj.alertc);
     document.getElementById("alert").classList.remove("invisible");
 }
-
